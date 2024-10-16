4a8eb774ea75ba14039b4bbed35e0532bc3db582
Thomas GUFFROY <thomas.guffroy@telecom-sudparis.eu>
2024-10-16 17:03:32 +0200

    Adding sine oscillator

--- drum-machine.js
+++ drum-machine.js
@@ -2,6 +2,9 @@ console.log("This is my drum machine");
 
 document.addEventListener('DOMContentLoaded', function() {
 
+
+    const c = new AudioContext()
+
     const kick = new Audio("assets/kick.wav");
 
     const samples = [
@@ -12,11 +15,28 @@ document.addEventListener('DOMContentLoaded', function() {
     const keys = document.querySelectorAll(".key")
     keys.forEach(key => {
         key.addEventListener('click', function() {
-        key_click(key.children[0], kick);
+            playNote(c, key.children[0])
+            key_click(key.children[0], kick);
         })
     })
 
 });
+
+
+playNote = function(c, element) {
+    if(element.classList.contains("led-off")){
+        element.classList.remove("led-off");
+        element.classList.add("led-on");
+        o = c.createOscillator()
+        o.connect(c.destination)
+        o.start()
+        setTimeout(function() {o.stop()}, 500)
+    }
+    else{
+        element.classList.remove("led-on");
+        element.classList.add("led-off");
+    }
+}
 
 
 key_click = function(element, sample){
