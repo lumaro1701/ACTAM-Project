d539fa2d3a28c189558027d6ad1f4b48056ebee7
Thomas GUFFROY <thomas.guffroy@telecom-sudparis.eu>
2024-10-18 16:21:34 +0200

    Adding buttons, samples et synth/drum machine mode

--- drum-machine.js
+++ drum-machine.js
@@ -1,53 +1,85 @@
 console.log("This is my drum machine");
 
+bpm = 120
+beat = 60/bpm
+
+mode = 0
+play = 0
+octave = 3
+
 document.addEventListener('DOMContentLoaded', function() {
 
 
-    const c = new AudioContext()
-
-    const kick = new Audio("assets/kick.wav");
+    const audio_context = new AudioContext()
 
     const samples = [
         new Audio("assets/kick.wav"),
+        new Audio("assets/snare.wav"),
+        new Audio("assets/closed_hihat.wav"),
+        new Audio("assets/open_hihat.wav"),
+        new Audio("assets/shaker.wav"),
+        new Audio("assets/fill.wav"),
     ]
 
-
+    //keys click
     const keys = document.querySelectorAll(".key")
-    keys.forEach(key => {
+    keys.forEach((key, index) => {
         key.addEventListener('click', function() {
-            playNote(c, key.children[0])
-            key_click(key.children[0], kick);
+
+            if(mode == 0){
+                play_sample(key, samples[index])
+            }else{
+                play_note(key, audio_context, index, octave)
+            }
+            
         })
     })
+
+
+    const mode_button = document.querySelector("#mode_button")
+    mode_button.addEventListener('click', function() {
+        mode = (mode+1) % 2
+    })
+
+
+    const play_button = document.querySelector("#play_button")
+    play_button.addEventListener('click', function() {
+        play = (play+1) % 2
+        play_seq(keys)
+    })
+
+
 
 });
 
 
-playNote = function(c, element) {
-    if(element.classList.contains("led-off")){
-        element.classList.remove("led-off");
-        element.classList.add("led-on");
-        o = c.createOscillator()
-        o.connect(c.destination)
-        o.start()
-        setTimeout(function() {o.stop()}, 500)
-    }
-    else{
-        element.classList.remove("led-on");
-        element.classList.add("led-off");
-    }
+
+led_on = function(led) {
+    led.classList.remove("led-off");
+    led.classList.add("led-on");
+    setTimeout(function() {
+        led.classList.remove("led-on");
+        led.classList.add("led-off");
+    }, beat/8*1000)
 }
 
 
-key_click = function(element, sample){
-    if(element.classList.contains("led-off")){
-        element.classList.remove("led-off");
-        element.classList.add("led-on");
-        state = sample.play();
-    }
-    else{
-        element.classList.remove("led-on");
-        element.classList.add("led-off");
-        sample.pause();
-    }
+play_note = function(key, audio_context, key_index, octave) {
+    led_on(key.children[0])
+    osc = audio_context.createOscillator()
+    osc.connect(audio_context.destination)
+    osc.frequency.setValueAtTime((32.70*2**(key_index/12+octave)), audio_context.currentTime)
+
+    osc.start()
+    setTimeout(function() {osc.stop()}, 200)
+}
+
+
+play_sample = function(key, sample){
+    led_on(key.children[0])
+    sample.play()
+}
+
+
+play_seq = function(keys) {
 }
