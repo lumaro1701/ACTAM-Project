86f73479c31aec7665a52a1382a9854831f32fa0
Thomas GUFFROY <thomas.guffroy@telecom-sudparis.eu>
2024-10-29 00:45:24 +0100

    Adding the ability to select a sample, create & play a sequence of samples.

--- drum-machine.js
+++ drum-machine.js
@@ -1,11 +1,11 @@
 console.log("This is my drum machine");
 
-bpm = 120
+bpm = 174
 beat = 60/bpm
 
 mode = 0
 play = 0
-edit_mode = 0
+edit_mode = -1
 
 octave = 3
 
@@ -24,7 +24,14 @@ const samples = [
 ]
 
 //Step list for each sample
-const sample_seqs = Array(samples.length).fill(Array(nb_keys).fill(false))
+sample_seqs = Array(nb_keys)
+for(let i=0; i<sample_seqs.length; i++){
+    sample_seqs[i] = Array(nb_keys)
+    for(let j=0; j<nb_keys; j++){
+        sample_seqs[i][j] = 0
+    }
+}
+
 
 //Waiting for the HTML content to be loaded
 document.addEventListener('DOMContentLoaded', function() {
@@ -40,19 +47,15 @@ document.addEventListener('DOMContentLoaded', function() {
     keys.forEach((key, index) => {
         key.addEventListener('click', function() {
 
-
             //Play mode
-            if (edit_mode == 0) {
-                if(mode == 0){ //drum mode
-                    play_sample(key, index)
-                }else{ //synth mode
-                    play_note(key, index, octave)
-                }
+            if (edit_mode == -1) {
+                key_clicked(key, index)
             }
 
             //Edit mode
             else {
-                //to be filled
+                edit_sample_seq(index, edit_mode)
+                play_sample(edit_mode)
             }
             
         })
@@ -77,13 +80,11 @@ document.addEventListener('DOMContentLoaded', function() {
         play = 1 - play
     })
 
-    //Selector
-    const selectors = document.querySelectorAll(".select-button-enabled")
+    //Selector and edit mode
+    const selectors = document.querySelectorAll(".select-button")
     selectors.forEach((selector, index) => {
         selector.addEventListener('click', function (){
-
-            //to be filled
-
+            toggle_edit_mode(index)
         })
     })
 
@@ -100,6 +101,7 @@ counter = 0
 switch_mode = function(){
     keys = document.querySelectorAll(".key")
     keys.forEach(key => key.classList.toggle("key-white"))
+    toggle_edit_mode(-1)
 }
 
 one_led_on = function(led, keep_on=false) {
@@ -122,6 +124,21 @@ render = function() {
     leds[counter].classList.add("led-on")
 }
 
+render_leds_edit = function(edit_sample_index) {
+    all_led_off()
+    steps_on = sample_seqs[edit_sample_index]
+    keys = document.querySelectorAll(".key")
+    keys.forEach((key, index) => {
+        if (steps_on[index] == 1){
+            one_led_on(key.children[0], true)
+        }
+    })
+}
+
+disable_all_select_buttons = function () {
+    document.querySelectorAll(".select-button").forEach(button => button.style.backgroundColor = "")
+}
+
 create_key = function(index) {
     //Create key
     e = document.createElement("div")
@@ -134,9 +151,6 @@ create_key = function(index) {
     //Create selector
     e = document.createElement("div")
     e.classList.add("select-button")
-    if (index < samples.length) {
-        e.classList.add("select-button-enabled")
-    }
     document.querySelector(".selectors").appendChild(e)
 }
 
@@ -148,10 +162,16 @@ intervalId = 0
 incr = function () {
     render()
     counter = (counter+1) % nb_keys
+    for(let i=0; i<sample_seqs.length; i++){
+        if (sample_seqs[i][counter] == 1){
+            play_sample(i)
+        }
+    }
 }
 
 play_seq = function () {
     intervalId = setInterval(incr, beat/4*1000)
+    toggle_edit_mode(-1)
 }
 
 stop_seq = function () {
@@ -160,11 +180,35 @@ stop_seq = function () {
     counter = 0
 }
 
+edit_sample_seq = function(key_index, sample_index) {
+    sample_seqs[sample_index][key_index] = 1 - sample_seqs[sample_index][key_index]
+    render_leds_edit(sample_index)
+}
 
+toggle_edit_mode = function(index) {
+    disable_all_select_buttons()
+    if(edit_mode == index || index == -1){
+        edit_mode = -1
+        all_led_off()
+    }else{
+        edit_mode = index
+        render_leds_edit(edit_mode)
+        document.querySelectorAll(".select-button")[index].style.backgroundColor = "#ff0000"
+    }
+}
+
+
+key_clicked = function(key, key_index) {
+    one_led_on(key.children[0])
+    if(mode == 0){
+        play_sample(key_index)
+    }else{
+        play_note(key, key_index)
+    }
+}
 
 //MIX
-play_note = function(key, key_index, octave) {
-    one_led_on(key.children[0])
+play_note = function(key, key_index) {
     osc = audio_context.createOscillator()
     osc.connect(audio_context.destination)
     osc.frequency.setValueAtTime((32.70*2**(key_index/12+octave)), audio_context.currentTime)
@@ -174,9 +218,8 @@ play_note = function(key, key_index, octave) {
 }
 
 
-play_sample = function(key, sample_index){
+play_sample = function(sample_index){
     if(sample_index < samples.length){
-        one_led_on(key.children[0])
         samples[sample_index].play()
     }
 }
