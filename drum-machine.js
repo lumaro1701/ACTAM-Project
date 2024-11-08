b325f7f1bf53ab759024b76938f1c91d37249449
Thomas GUFFROY <thomas.guffroy@telecom-sudparis.eu>
2024-11-08 13:04:30 +0100

    Changing the script to a module

--- drum-machine.js
+++ drum-machine.js
@@ -1,6 +1,6 @@
-/*
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,22 +15,21 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-db = firebase.firestore();
-*/
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 console.log("This is my drum machine");
 
-bpm = 174
-beat = 60/bpm
+let bpm = 174
+let beat = 60/bpm
 
-mode = 0
-play = 0
-edit_mode = -1
+let mode = 0
+let play = 0
+let edit_mode = -1
 
-octave = 3
+let octave = 3
 
-nb_keys = 16
+let nb_keys = 16
 
 const audio_context = new AudioContext()
 
@@ -45,7 +44,7 @@ const samples = [
 ]
 
 //Step list for each sample
-sample_seqs = Array(nb_keys)
+let sample_seqs = Array(nb_keys)
 for(let i=0; i<sample_seqs.length; i++){
     sample_seqs[i] = Array(nb_keys)
     for(let j=0; j<nb_keys; j++){
@@ -56,7 +55,6 @@ for(let i=0; i<sample_seqs.length; i++){
 /*
 db.collection("store").doc("data").set({nb_keys: nb_keys})
 */
-
 
 //Waiting for the HTML content to be loaded
 document.addEventListener('DOMContentLoaded', function() {
@@ -118,18 +116,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 //MODEL
-counter = 0
+let counter = 0
 
 
 
 //VIEW
-switch_mode = function(){
-    keys = document.querySelectorAll(".key")
+function switch_mode(){
+    let keys = document.querySelectorAll(".key")
     keys.forEach(key => key.classList.toggle("key-white"))
     toggle_edit_mode(-1)
 }
 
-one_led_on = function(led, keep_on=false) {
+function one_led_on(led, keep_on=false) {
     led.classList.toggle("led-on");
     if (!keep_on) {
         setTimeout(function() {
@@ -138,21 +136,21 @@ one_led_on = function(led, keep_on=false) {
     }
 }
 
-all_led_off = function() {
-    leds = document.querySelectorAll(".led")
+function all_led_off() {
+    let leds = document.querySelectorAll(".led")
     leds.forEach(led => led.classList.remove("led-on"))
 }
 
-render = function() {
-    leds = document.querySelectorAll(".led")
+function render() {
+    let leds = document.querySelectorAll(".led")
     all_led_off()
     leds[counter].classList.add("led-on")
 }
 
-render_leds_edit = function(edit_sample_index) {
+function render_leds_edit(edit_sample_index) {
     all_led_off()
-    steps_on = sample_seqs[edit_sample_index]
-    keys = document.querySelectorAll(".key")
+    let steps_on = sample_seqs[edit_sample_index]
+    let keys = document.querySelectorAll(".key")
     keys.forEach((key, index) => {
         if (steps_on[index] == 1){
             one_led_on(key.children[0], true)
@@ -160,15 +158,15 @@ render_leds_edit = function(edit_sample_index) {
     })
 }
 
-disable_all_select_buttons = function () {
+function disable_all_select_buttons() {
     document.querySelectorAll(".select-button").forEach(button => button.style.backgroundColor = "")
 }
 
-create_key = function(index) {
+function create_key(index) {
     //Create key
-    e = document.createElement("div")
+    let e = document.createElement("div")
     e.classList.add("key")
-    k = document.createElement("div")
+    let k = document.createElement("div")
     k.classList.add("led")
     e.appendChild(k)
     document.querySelector(".keyboard").appendChild(e)
@@ -182,9 +180,9 @@ create_key = function(index) {
 
 
 //CONTROLLER
-intervalId = 0
+let intervalId = 0
 
-incr = function () {
+function incr() {
     render()
     counter = (counter+1) % nb_keys
     for(let i=0; i<sample_seqs.length; i++){
@@ -194,23 +192,23 @@ incr = function () {
     }
 }
 
-play_seq = function () {
+function play_seq() {
     intervalId = setInterval(incr, beat/4*1000)
     toggle_edit_mode(-1)
 }
 
-stop_seq = function () {
+function stop_seq() {
     clearInterval(intervalId)
     all_led_off()
     counter = 0
 }
 
-edit_sample_seq = function(key_index, sample_index) {
+function edit_sample_seq(key_index, sample_index) {
     sample_seqs[sample_index][key_index] = 1 - sample_seqs[sample_index][key_index]
     render_leds_edit(sample_index)
 }
 
-toggle_edit_mode = function(index) {
+function toggle_edit_mode(index) {
     disable_all_select_buttons()
     if(edit_mode == index || index == -1){
         edit_mode = -1
@@ -219,11 +217,12 @@ toggle_edit_mode = function(index) {
         edit_mode = index
         render_leds_edit(edit_mode)
         document.querySelectorAll(".select-button")[index].style.backgroundColor = "#ff0000"
+        //document.querySelectorAll(".select-button")[index].textContent = "S"
     }
 }
 
 
-key_clicked = function(key, key_index) {
+function key_clicked(key, key_index) {
     one_led_on(key.children[0])
     if(mode == 0){
         play_sample(key_index)
@@ -233,8 +232,8 @@ key_clicked = function(key, key_index) {
 }
 
 //MIX
-play_note = function(key, key_index) {
-    osc = audio_context.createOscillator()
+function play_note(key, key_index) {
+    let osc = audio_context.createOscillator()
     osc.connect(audio_context.destination)
     osc.frequency.setValueAtTime((32.70*2**(key_index/12+octave)), audio_context.currentTime)
 
@@ -243,7 +242,7 @@ play_note = function(key, key_index) {
 }
 
 
-play_sample = function(sample_index){
+function play_sample(sample_index){
     if(sample_index < samples.length){
         samples[sample_index].play()
     }
